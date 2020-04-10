70d5fed47820b3c7fd241dceaf44605ee192c11a
Gary <gary909@gmail.com>
2020-04-10 09:27:08 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need.
+
+// Example:
+// paperwork(5, 5) == 25
+// Note: if n < 0 or m < 0 return 0!
+
+
+// function paperwork(n, m) {
+
+// }
+
+
+// Test.assertEquals(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
+
+
+
+function paperwork(n, m) {
+    if (n < 0 || m < 0) {
+        return 0;
+    } else {
+        return n * m;
+    }
+}
+
+console.log(paperwork(5, 5));
